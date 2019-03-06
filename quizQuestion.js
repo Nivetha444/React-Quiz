57aa036238c114f91eba88fca1b1cd9539690fcc
Nivetha444 <30614697+Nivetha444@users.noreply.github.com>
2019-03-06 12:00:36 +0530

    Add files via upload
    MyApp -> src -> API -> quizQuestion.js
    This js file is for the list of questions in the quiz.

---
+++ quizQuestion.js
@@ -0,0 +1,78 @@
+
+
+var questionQuiz = [
+    {
+    question: "National bird of India?",
+    answers: [
+    {
+        type: "a",
+        content:"Crow"
+    },
+    {
+        type: "b",
+        content:"Peacock"
+    },
+    {
+        type: "a",
+        content:"Sparrow"
+    }
+    ]
+
+},
+
+{
+question: "Capital of China?",
+    answers: [
+    {
+        type: "a",
+        content:"Beijing"
+    },
+    {
+        type: "b",
+        content:"Seoul"
+    },
+    {
+        type: "a",
+        content:"Bangkok"
+    },
+    ]
+},
+
+{
+    question: "Does Indai has Union Terriotries?",
+        answers: [
+        {
+            type: "a",
+            content:"Yes"
+        },
+        {
+            type: "b",
+            content:"No"
+        },
+        {
+            type: "a",
+            content:"Not Sure"
+        },
+        ]
+    },
+
+    {
+        question: "Where is Statue of Liberty located?",
+            answers: [
+            {
+                type: "a",
+                content:"New York"
+            },
+            {
+                type: "b",
+                content:"Germany"
+            },
+            {
+                type: "a",
+                content:"France"
+            },
+            ]
+        },
+];
+
+export default questionQuiz;
